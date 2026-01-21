cdc60563c7eeb0a5f171c3b11a34dfbed71c4af9
Saurabh <sauravthakulla683@gmail.com>
2026-01-21 23:02:36 +0530

    ADD : Navigation bar globally set

---
+++ my-app/app/navigation.tsx
@@ -0,0 +1,17 @@
+export default function Navigation() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="/about">About</a></li>
+                </ul>
+                <ul>
+                    <li><a href="/about/services">services</a></li>
+                </ul>
+                <ul>
+                    <li><a href="/about/services/end">End</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
